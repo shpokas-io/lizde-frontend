7a03cdb7e0ee6d360ef61f7d7f58f833c4866579
shpokas <s.shpokas@gmail.com>
2025-01-12 16:43:51 +0200

    feat(hero): add background dinamic AspectRatio and Padding-height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/landingPage/HeroSection";
+
 export default function HomePage() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, World!</h1>
+    <div>
+      <HeroSection />
     </div>
   );
 }
---
+++ src/components/landingPage/HeroSection.tsx
@@ -0,0 +1,28 @@
+import Button from "../reusable/Button";
+
+export default function HeroSection() {
+  return (
+    <section
+      className="relative bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/shaded.png')" }}
+    >
+      {/* Aspect Ratio or Padding-Based Height */}
+      <div className="pt-[50%] sm:pt-[60%] md:pt-[30%] lg:pt-[25%] relative">
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+
+        {/* Content */}
+        <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center text-white">
+          <h1 className="text-4xl md:text-6xl font-bold mb-4">
+            Become a Sound Engineer
+          </h1>
+          <p className="text-lg md:text-xl mb-6 max-w-2xl">
+            Achieve professional-quality recordings and elevate your sound
+            engineering skills with expert guidance.
+          </p>
+          <Button text="Explore Courses" href="/courses" />
+        </div>
+      </div>
+    </section>
+  );
+}
