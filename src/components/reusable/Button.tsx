bc8622353cfb42abccead0596f9e60dc73279b64
shpokas <s.shpokas@gmail.com>
2025-01-12 16:26:59 +0200

    fix(button): syntax fix for button component

--- src/components/reusable/Button.tsx
+++ src/components/reusable/Button.tsx
@@ -4,17 +4,17 @@ export default function Button({
   text,
   href,
   className = "",
-} {
+}: {
   text: string;
   href: string;
   className?: string;
 }) {
   return (
     <Link
-    href={href}
-    className={`px-6 py-3 bg-blue-600 text-white text-lg rounded-lg hover:bg-blue-700 transition-colors ${className}`}
+      href={href}
+      className={`px-6 py-3 bg-blue-600 text-white text-lg rounded-lg hover:bg-blue-700 transition-colors ${className}`}
     >
       {text}
     </Link>
-  )
+  );
 }
