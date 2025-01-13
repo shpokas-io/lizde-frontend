e5e60b9771c2a81a718daca3cb8675ba5feaac43
shpokas <s.shpokas@gmail.com>
2025-01-13 20:38:59 +0200

    feat(hero): add HeroSection with background and content

--- src/components/landingPage/HeroContent.tsx
+++ src/components/landingPage/HeroContent.tsx
@@ -4,10 +4,9 @@ const HeroContent = () => {
   return (
     <div className="relative z-10 text-center text-white max-w-2xl mx-auto flex flex-col items-center">
       <h1 className="text-4xl md:text-6xl font-bold mb-4 leading-tight">
-        Become a Full Stack Producer, Achieve Pro-Quality Mixes and Get Paid to
-        Record & Mix
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
       </h1>
-      <Button text="Get Free Access Now" href="/free-training" />
+      <Button text="Lorem ipsum" href="/free-training" />
     </div>
   );
 };
--- src/components/landingPage/HeroSection.tsx
+++ src/components/landingPage/HeroSection.tsx
@@ -1,29 +1,14 @@
-import Button from "../reusable/Button";
+import HeroBackground from "./HeroBackground";
+import HeroContent from "./HeroContent";
 
-export default function HeroSection() {
+const HeroSection = () => {
   return (
-    <section
-      className="relative bg-cover bg-center"
-      style={{ backgroundImage: "url('/images/shaded.png')" }}
-    >
-      {/* TODO: Find better solution for below comment  */}
-      {/* Aspect Ratio or Padding-Based Height */}
-      <div className="pt-[70%] sm:pt-[60%] md:pt-[30%] lg:pt-[25%] relative">
-        {/* Overlay */}
-        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-
-        {/* Content */}
-        <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center text-white">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4">
-            Become a Sound Engineer
-          </h1>
-          <p className="text-lg md:text-xl mb-6 max-w-2xl">
-            Achieve professional-quality recordings and elevate your sound
-            engineering skills with expert guidance.
-          </p>
-          <Button text="Explore Courses" href="/courses" />
-        </div>
-      </div>
+    <section className="relative h-[80vh] md:h-[90vh] flex items-center justify-center">
+      <HeroBackground imageUrl="/images/hero-image.jpg" />
+      <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+      <HeroContent />
     </section>
   );
-}
+};
+
+export default HeroSection;
