901038ac14d5bd0a6ca29ce8c5731da6a0b7fd9f
shpokas <s.shpokas@gmail.com>
2025-01-13 20:32:16 +0200

    feat(btn): update reusable btn component

--- src/components/reusable/Button.tsx
+++ src/components/reusable/Button.tsx
@@ -1,20 +1,19 @@
 import Link from "next/link";
 
-export default function Button({
-  text,
-  href,
-  className = "",
-}: {
+interface ButtonProps {
   text: string;
   href: string;
   className?: string;
-}) {
+}
+
+const Button = ({ text, href, className = "" }: ButtonProps) => {
   return (
     <Link
       href={href}
-      className={`px-6 py-3 bg-blue-600 text-white text-lg rounded-lg hover:bg-blue-700 transition-colors ${className}`}
+      className={`px-6 py-3 bg-orange-500 text-white text-lg font-bold rounded hover:bg-orange-600 transition ${className}`}
     >
       {text}
     </Link>
   );
-}
+};
+export default Button;
