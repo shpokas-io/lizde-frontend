a6c5a07bead6eeb6be82b7af5d5c6e51d0cc474f
shpokas <s.shpokas@gmail.com>
2025-01-14 19:47:03 +0200

    chore: Integrate AboutSection into HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutSection from "@/components/landingPage/Sections/about-section/AboutSection";
 import HeroSection from "@/components/landingPage/Sections/hero-section/HeroSection";
 
 export default function HomePage() {
   return (
     <div>
       <HeroSection />
+      <AboutSection />
     </div>
   );
 }
