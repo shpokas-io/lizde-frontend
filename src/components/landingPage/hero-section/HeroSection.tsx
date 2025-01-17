cdf74f249a8617f15a325fdebc4d17c8e6a80813
shpokas <s.shpokas@gmail.com>
2025-01-17 17:09:59 +0200

    feat(youtube): viewport fix

--- src/components/landingPage/hero-section/HeroSection.tsx
+++ src/components/landingPage/hero-section/HeroSection.tsx
@@ -3,7 +3,7 @@ import HeroContent from "./HeroContent";
 
 const HeroSection = () => {
   return (
-    <section className="relative h-[80vh] md:h-[70vh] flex items-center justify-center">
+    <section className="relative h-[80vh] md:h-[100vh] flex items-center justify-center">
       <HeroBackground imageUrl="/images/hero-background.jpg" />
       <div className="absolute inset-0 bg-black bg-opacity-40"></div>
       <HeroContent />
