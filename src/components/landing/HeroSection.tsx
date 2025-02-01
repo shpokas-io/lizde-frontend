3530a64ca9fff9a6b49f7247293d08aee570caaf
shpokas <s.shpokas@gmail.com>
2025-02-01 18:38:39 +0200

    fix: mobile viewport size for heroSection

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -2,26 +2,30 @@ import Button from "@/components/common/Button";
 
 const HeroSection = () => {
   return (
-    <section className="relative md:h-[80vh] flex items-center justify-center">
+    <section className="relative h-[90vh] md:h-[80vh] flex items-center justify-center">
       {/* Background Image */}
       <div
-        className="absolute inset-2 bg-cover bg-center"
+        className="absolute inset-0 bg-cover bg-center"
         style={{ backgroundImage: "url('/images/hero-background.jpg')" }}
       />
 
       {/* Overlay Effect */}
-      <div className="absolute inset-0 bg-black bg-opacity-40" />
+      <div className="absolute inset-0 bg-black bg-opacity-50" />
 
       {/* Hero Content */}
-      <div className="relative z-10 text-center text-white max-w-2xl mx-auto flex flex-col items-center">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4 leading-tight">
+      <div className="relative z-10 text-center text-white max-w-2xl mx-auto flex flex-col items-center pt-10">
+        <h1 className="text-5xl md:text-6xl font-bold mb-6 leading-tight">
           Tavo garsai
           <br />
           Tavo taisyklės
           <br />
           Mūsų žinios
         </h1>
-        <Button className="mt-4" text="Pradėk dabar" href="/courses" />
+        <Button
+          className="mt-6 px-6 py-3 text-lg"
+          text="Pradėk dabar"
+          href="/courses"
+        />
       </div>
     </section>
   );
